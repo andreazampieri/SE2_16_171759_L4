dc04c41879ebed842f0bcd07e9da91aa18bff2f7
andreazampieri <andreazampieri95@gmail.com>
2016-11-18 23:10:11 +0100

    added functions in scripts/employees.js

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var express = require('express');
 
 // initialization server side
 var app = express();
+var address = '127.0.0.1';
 var port = 32123;
 
 app.set('port',(process.env.PORT || port));
-app.listen('localhost',port);
+app.listen(address,port);
 
 // actions to be done on a request on the root path
-app.use('/',function() {
+app.use('/',function(request,response) {
 	// TODO
 });
 
+console.log("Running on ",address,":",port,"\nhttp://",address,":",port);
