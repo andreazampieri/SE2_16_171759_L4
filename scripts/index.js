0db69d8bb366dd958bf637180b7b668e7c7fa6bd
andreazampieri <andreazampieri95@gmail.com>
2016-11-23 14:10:36 +0100

    implemented deletion and update of an employee

--- scripts/index.js
+++ scripts/index.js
@@ -20,48 +20,51 @@ console.log('Server started on',address,':',port);
 //serve scripts
 app.use('/scripts', express.static('scripts'));
 
+var headers = {};
+headers["Access-Control-Allow-Origin"] = "*"; //for cross enviroment request
+headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";//methods allowed to responce
+headers["Access-Control-Allow-Credentials"] = false;
+headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"; //type of headers
+
+
 app.use('/insert',function(request,response){
+	response.writeHead(200,headers);
 	var id = request.body.id != '' ? parseInt(request.body.id) : employees.getNewID();
 	var name = request.body.name != '' ? request.body.name : 'undefined';
 	var surname = request.body.surname != '' ? request.body.surname : 'undefined';
 	var level = request.body.level != '' ? parseInt(request.body.level) : 'undefined';
 	var salary = request.body.salary != '' ? parseInt(request.body.salary) : 0;
 
-	employees.addEmployee(id,name,surname,salary);
-	bind.toFile('templates/index.tpl',
-		{},
-		function(data){
-			response.writeHead(200,{'Content-Type':'text/html'});
-			response.end(data);
-		});
-	console.log('insert');
+	employees.addEmployee(parseInt(id),name,surname,parseInt(level),parseInt(salary));
+
+	response.end();
 });
 
 
 app.use('/search',function(request,response){
-	var headers = {};
-    headers["Access-Control-Allow-Origin"] = "*"; //for cross enviroment request
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";//methods allowed to responce
-    headers["Access-Control-Allow-Credentials"] = false;
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"; //type of headers
-    //answer
-    headers["Content-Type"] = "application/json";//format response
-	response.writeHead(200, headers);
+	var head = headers;
+   	//answer
+    head["Content-Type"] = "application/json";//format response
+	response.writeHead(200, head);
 
 	var id = request.body.id;
-	console.log('searching '+id);	
 	if(employees.isPresent(parseInt(id))){
 		var emp = employees.getEmployee(parseInt(id));		
 		response.end(JSON.stringify(emp));
 	}else{
-		console.log('invalid id');
 		response.end(JSON.stringify(null));
 	}
 });
 
 app.use('/delete',function(request,response){
+	response.writeHead(200,headers);
 
+	id = request.body.id;
+	if(employees.isPresent(parseInt(id))){
+		employees.removeEmployee(parseInt(id));
+	}
+	response.end();
 });
 
 // actions to be done on a request on the root path
--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,20 @@ function toggleForm(){
 }
 
 function searchEmp(){
-	var id = document.getElementById('idsearch');
+	var id = document.getElementById('id');
+	var name = document.getElementById('name');
+	var surname = document.getElementById('surname');
+	var level = document.getElementById('level');
+	var salary = document.getElementById('salary');
+	var param = document.getElementById('idsearch');
 	var form = document.getElementById('input-form');
 	var xhr = new XMLHttpRequest();
 	xhr.open('post','http://localhost:32123/search',true);
 	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xhr.send('id='+id.value);
+	xhr.send('id='+param.value);
 	xhr.onloadend = function(){
 		var result = JSON.parse(xhr.responseText);
 		if (result != null){
-			var id = document.getElementById('id');
-			var name = document.getElementById('name');
-			var surname = document.getElementById('surname');
-			var level = document.getElementById('level');
-			var salary = document.getElementById('salary');
-	
 			form.style.display = 'block';
 			id.value = result['id'];
 			name.value = result['name'];
@@ -27,10 +26,56 @@ function searchEmp(){
 			salary.value = result['salary'];
 		}else{
 			form.style.display = 'none';
+			id.value = '';
+			name.value = '';
+			surname.value = '';
+			level.value = '';
+			salary.value = '';
 		}
 	}
 }
 
 function deleteEmp(){
+	var id = document.getElementById('id');
+	var name = document.getElementById('name');
+	var surname = document.getElementById('surname');
+	var level = document.getElementById('level');
+	var salary = document.getElementById('salary');
+	var form = document.getElementById('input-form');
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('post','http://localhost:32123/delete',true);
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.send('id='+id.value);
+	xhr.onloadend = function(){
+		form.style.display = 'none';
+
+		id.value = '';
+		name.value = '';
+		surname.value = '';
+		level.value = '';
+		salary.value = '';
+	}	
+}
+
+function insertEmp(){
+	var id = document.getElementById('id');
+	var name = document.getElementById('name');
+	var surname = document.getElementById('surname');
+	var level = document.getElementById('level');
+	var salary = document.getElementById('salary');
 	
+	var form = document.getElementById('input-form');
+	var xhr = new XMLHttpRequest();
+	xhr.open('post','http://localhost:32123/insert',true);
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.send('id='+id.value+'&name='+name.value+'&surname='+surname.value+'&level='+level.value+'&salary='+salary.value);
+	xhr.onloadend = function(){
+		form.style.display = 'none';
+		id.value = '';
+		name.value = '';
+		surname.value = '';
+		level.value = '';
+		salary.value = '';
+	}	
 }
